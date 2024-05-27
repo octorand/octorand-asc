f7ce0647fde9f4c4413cc63d0178bba2a3ef6cf8
Declan Rocco <declanrocco@protonmail.com>
2024-05-27 19:50:56 +1000

    Test new logs

--- src/app/test.js
+++ src/app/test.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 
 (async () => {
 
-    await require('./command/gen1/design/00_compile_contract').execute();
-    await require('./command/gen1/design/01_create_application').execute();
-    await require('./command/gen1/design/02_update_application').execute();
+    // await require('./command/gen1/design/00_compile_contract').execute();
+    // await require('./command/gen1/design/01_create_application').execute();
+    // await require('./command/gen1/design/02_update_application').execute();
 
-    await require('./command/gen1/market/00_compile_contract').execute();
-    await require('./command/gen1/market/01_create_application').execute();
-    await require('./command/gen1/market/02_update_application').execute();
+    // await require('./command/gen1/market/00_compile_contract').execute();
+    // await require('./command/gen1/market/01_create_application').execute();
+    // await require('./command/gen1/market/02_update_application').execute();
 
-    await require('./command/gen1/vault/00_compile_contract').execute();
-    await require('./command/gen1/vault/01_create_application').execute();
-    await require('./command/gen1/vault/02_update_application').execute();
+    // await require('./command/gen1/vault/00_compile_contract').execute();
+    // await require('./command/gen1/vault/01_create_application').execute();
+    // await require('./command/gen1/vault/02_update_application').execute();
 
-    await require('./command/gen1/wallet/00_compile_contract').execute();
-    await require('./command/gen1/wallet/01_create_application').execute();
-    await require('./command/gen1/wallet/02_update_application').execute();
+    // await require('./command/gen1/wallet/00_compile_contract').execute();
+    // await require('./command/gen1/wallet/01_create_application').execute();
+    // await require('./command/gen1/wallet/02_update_application').execute();
 
     // await require('./command/gen1/storage/00_compile_contract').execute();
     // await require('./command/gen1/storage/01_setup_prime_input').execute();
@@ -30,11 +30,10 @@ require('dotenv').config();
     // await require('./command/gen1/storage/09_transfer_assets').execute();
     // await require('./command/gen1/storage/10_lock_prime_asset').execute();
     // await require('./command/gen1/storage/11_update_application').execute();
-    // await require('./command/gen1/storage/12_read_application_state').execute();
 
     // await require('./command/gen1/wallet/03_call_upgrade').execute();
     // await require('./command/gen1/wallet/04_call_mint').execute();
     // await require('./command/gen1/wallet/05_call_withdraw').execute();
-    // await require('./command/gen1/wallet/06_read_application_logs').execute();
+    await require('./command/gen1/wallet/06_read_application_logs').execute();
 
 })();
