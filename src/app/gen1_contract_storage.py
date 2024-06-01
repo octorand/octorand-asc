6e8c055a8bd9d9a813ffc52686ad3ddd7c96cf71
Declan Rocco <declanrocco@protonmail.com>
2024-06-01 19:52:21 +1000

    Add setup contracts

--- src/app/gen1_contract_storage.py
+++ src/app/gen1_contract_storage.py
@@ -1,6 +1,5 @@
 import func
 import gen1_const
-import gen1_contract_storage as storage
 
 from pyteal import *
 from typing import *
@@ -13,9 +12,7 @@ prime = gen1_const.Prime()
 @Subroutine(TealType.none)
 def create():
     return Seq(
-        Assert(Txn.sender() == const.manager_address),
-        func.init_global(prime.key_1),
-        func.init_global(prime.key_2),
+        Reject(),
     )
 
 
@@ -40,67 +37,6 @@ router = Router(
     ),
     clear_state=Approve(),
 )
-
-
-@router.method
-def initialize(
-    id: abi.Uint64,
-    prime_asset: abi.Asset,
-    legacy_asset: abi.Asset,
-):
-    return Seq(
-        Assert(Txn.sender() == const.admin_address),
-        prime.id.set(id.get()),
-        prime.prime_asset_id.set(prime_asset.asset_id()),
-        prime.legacy_asset_id.set(legacy_asset.asset_id()),
-        prime.price.set(Int(0)),
-        prime.seller.set(Global.zero_address()),
-        func.optin_into_asset(const.platform_asset_id),
-        func.optin_into_asset(prime_asset.asset_id()),
-        func.optin_into_asset(legacy_asset.asset_id()),
-    )
-
-
-@router.method
-def populate(
-    theme: abi.Uint64,
-    skin: abi.Uint64,
-    is_founder: abi.Uint64,
-    is_artifact: abi.Uint64,
-    is_pioneer: abi.Uint64,
-    is_explorer: abi.Uint64,
-    name: abi.StaticBytes[Literal[8]],
-    description: abi.StaticBytes[Literal[64]],
-):
-    return Seq(
-        Assert(Txn.sender() == const.admin_address),
-        prime.theme.set(theme.get()),
-        prime.skin.set(skin.get()),
-        prime.is_founder.set(is_founder.get()),
-        prime.is_artifact.set(is_artifact.get()),
-        prime.is_pioneer.set(is_pioneer.get()),
-        prime.is_explorer.set(is_explorer.get()),
-        prime.name.set(name.get()),
-        prime.description.set(description.get()),
-    )
-
-
-@router.method
-def finalize(
-    score: abi.Uint64,
-    sales: abi.Uint64,
-    mints: abi.Uint64,
-    renames: abi.Uint64,
-    repaints: abi.Uint64,
-):
-    return Seq(
-        Assert(Txn.sender() == const.admin_address),
-        prime.score.set(score.get()),
-        prime.sales.set(sales.get()),
-        prime.mints.set(mints.get()),
-        prime.renames.set(renames.get()),
-        prime.repaints.set(repaints.get()),
-    )
 
 
 @router.method
--- src/app/run.py
+++ src/app/run.py
@@ -5,12 +5,14 @@ import env
 
 import gen1_contract_design
 import gen1_contract_market
+import gen1_contract_setup
 import gen1_contract_storage
 import gen1_contract_vault
 import gen1_contract_wallet
 
 import gen2_contract_design
 import gen2_contract_market
+import gen2_contract_setup
 import gen2_contract_storage
 import gen2_contract_vault
 import gen2_contract_wallet
@@ -19,12 +21,14 @@ import gen2_contract_wallet
 def run():
     write(*gen1_contract_design.router.compile_program(version=10), "gen1/design")
     write(*gen1_contract_market.router.compile_program(version=10), "gen1/market")
+    write(*gen1_contract_setup.router.compile_program(version=10), "gen1/setup")
     write(*gen1_contract_storage.router.compile_program(version=10), "gen1/storage")
     write(*gen1_contract_vault.router.compile_program(version=10), "gen1/vault")
     write(*gen1_contract_wallet.router.compile_program(version=10), "gen1/wallet")
 
     write(*gen2_contract_design.router.compile_program(version=10), "gen2/design")
     write(*gen2_contract_market.router.compile_program(version=10), "gen2/market")
+    write(*gen2_contract_setup.router.compile_program(version=10), "gen2/setup")
     write(*gen2_contract_storage.router.compile_program(version=10), "gen2/storage")
     write(*gen2_contract_vault.router.compile_program(version=10), "gen2/vault")
     write(*gen2_contract_wallet.router.compile_program(version=10), "gen2/wallet")
