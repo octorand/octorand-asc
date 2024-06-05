53d7bcf7c90fa500684561a4dff78e35a9e76be3
Declan Rocco <declanrocco@protonmail.com>
2024-06-05 10:50:12 +1000

    Generate prime details

--- src/deploy/testnet/gen1/run.js
+++ src/deploy/testnet/gen1/run.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 exports.execute = async function () {
 
-
+    console.log('gen1 prime setup resources');
+    await require('./prime/00_create_prime_inputs').execute();
 
 }
