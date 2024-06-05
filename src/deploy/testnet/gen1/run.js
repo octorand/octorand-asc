f083dbac860ee6664349cbb82cfd938f75f17e75
Declan Rocco <declanrocco@protonmail.com>
2024-06-06 09:29:42 +1000

    Create prime assets

--- src/deploy/testnet/gen1/run.js
+++ src/deploy/testnet/gen1/run.js
@@ -5,10 +5,10 @@ exports.execute = async function () {
     console.log('gen1 prime setup resources');
     await require('./prime/00_create_prime_inputs').execute();
     await require('./prime/01_create_legacy_assets').execute();
-    // await require('./prime/02_create_prime_assets').execute();
+    await require('./prime/02_create_prime_assets').execute();
 
     console.log('gen1 prime setup contracts');
-    // await require('./prime/03_create_main_applications').execute();
+    await require('./prime/03_create_main_applications').execute();
     // await require('./prime/04_update_main_applications').execute();
     // await require('./prime/05_fund_main_applications').execute();
     // await require('./prime/06_initialize_main_applications').execute();
