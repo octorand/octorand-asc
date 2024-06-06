5d06a9e21bf0ee3bfafc4801fc82d7f9bc66d488
Declan Rocco <declanrocco@protonmail.com>
2024-06-06 11:57:45 +1000

    Setup gen2 relations

--- src/deploy/testnet/gen2/run.js
+++ src/deploy/testnet/gen2/run.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 exports.execute = async function () {
 
-    console.log('gen1 prime setup resources');
+    console.log('gen2 prime setup resources');
     await require('./prime/00_create_prime_inputs').execute();
     // await require('./prime/01_create_legacy_assets').execute();
     // await require('./prime/02_create_prime_assets').execute();
 
-    console.log('gen1 prime setup contracts');
+    console.log('gen2 prime setup contracts');
     // await require('./prime/03_create_main_applications').execute();
     // await require('./prime/04_update_main_applications').execute();
     // await require('./prime/05_fund_main_applications').execute();
--- src/deploy/testnet/run.js
+++ src/deploy/testnet/run.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 
     await require('./setup/run').execute();
     await require('./gen1/run').execute();
-    // await require('./gen2/run').execute();
+    await require('./gen2/run').execute();
 
 })();
