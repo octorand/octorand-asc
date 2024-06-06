f5c97324beae3014a9b98c8364b45b3883f316ef
Declan Rocco <declanrocco@protonmail.com>
2024-06-06 19:09:45 +1000

    Combine prime function calls

--- src/deploy/testnet/gen2/prime/04_update_main_applications.js
+++ src/deploy/testnet/gen2/prime/04_update_main_applications.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const fs = require('fs');
 const testnet = require('./../../../../chain/testnet');
+const helpers = require('./../../../../chain/util/helpers');
 
 exports.execute = async function () {
     try {
@@ -11,6 +12,7 @@ exports.execute = async function () {
         let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.admin);
 
         let config = JSON.parse(fs.readFileSync('src/deploy/testnet/config.json'));
+        let contract = new connection.baseClient.ABIContract(JSON.parse(fs.readFileSync('src/build/testnet/gen2/prime/app/contract.json')));
 
         let version = 1;
 
@@ -19,11 +21,13 @@ exports.execute = async function () {
         for (let i = 0; i < max; i++) {
             let primes = config['gen2']['inputs']['primes'];
 
+            let composer = new connection.baseClient.AtomicTransactionComposer();
+
             if (primes[i]['application_version'] < version) {
+                primes[i]['application_version'] = version;
+
                 let approvalProgram = fs.readFileSync('src/build/testnet/gen2/prime/app/approval.teal', 'utf8');
                 let clearProgram = fs.readFileSync('src/build/testnet/gen2/prime/app/clear.teal', 'utf8');
-
-                let composer = new connection.baseClient.AtomicTransactionComposer();
 
                 composer.addTransaction({
                     signer: signer,
@@ -40,15 +44,124 @@ exports.execute = async function () {
                         }
                     })
                 });
+            }
 
+            if (!primes[i]['funded']) {
+                primes[i]['funded'] = true;
+
+                composer.addTransaction({
+                    signer: signer,
+                    txn: connection.baseClient.makePaymentTxnWithSuggestedParamsFromObject({
+                        from: sender,
+                        to: primes[i]['application_address'],
+                        amount: 400000,
+                        suggestedParams: {
+                            ...params,
+                            fee: 1000,
+                            flatFee: true
+                        }
+                    })
+                });
+            }
+
+            if (!primes[i]['initialized']) {
+                primes[i]['initialized'] = true;
+
+                composer.addMethodCall({
+                    sender: sender,
+                    signer: signer,
+                    appID: primes[i]['application_id'],
+                    method: helpers.method(contract, 'initialize'),
+                    methodArgs: [
+                        primes[i]['id'],
+                        config['setup']['platform']['asset_id'],
+                        primes[i]['prime_asset_id'],
+                        primes[i]['legacy_asset_id'],
+                        primes[i]['parent_application_id'],
+                    ],
+                    suggestedParams: {
+                        ...params,
+                        fee: 4000,
+                        flatFee: true
+                    }
+                });
+            }
+
+            if (!primes[i]['populated']) {
+                primes[i]['populated'] = true;
+
+                composer.addMethodCall({
+                    sender: sender,
+                    signer: signer,
+                    appID: primes[i]['application_id'],
+                    method: helpers.method(contract, 'populate'),
+                    methodArgs: [
+                        primes[i]['theme'],
+                        primes[i]['skin'],
+                        primes[i]['is_founder'],
+                        primes[i]['is_artifact'],
+                        primes[i]['is_pioneer'],
+                        primes[i]['is_explorer'],
+                        primes[i]['score'],
+                        primes[i]['sales'],
+                        primes[i]['drains'],
+                        primes[i]['transforms'],
+                        helpers.bytes(primes[i]['name'], 16),
+                    ],
+                    suggestedParams: {
+                        ...params,
+                        fee: 1000,
+                        flatFee: true
+                    }
+                });
+            }
+
+            if (!primes[i]['loaded']) {
+                primes[i]['loaded'] = true;
+
+                composer.addTransaction({
+                    sender: sender,
+                    signer: signer,
+                    txn: connection.baseClient.makeAssetTransferTxnWithSuggestedParamsFromObject({
+                        from: sender,
+                        to: primes[i]['application_address'],
+                        assetIndex: config['setup']['platform']['asset_id'],
+                        amount: 1000000,
+                        suggestedParams: {
+                            ...params,
+                            fee: 1000,
+                            flatFee: true
+                        }
+                    })
+                });
+
+                composer.addTransaction({
+                    sender: sender,
+                    signer: signer,
+                    txn: connection.baseClient.makePaymentTxnWithSuggestedParamsFromObject({
+                        from: sender,
+                        to: primes[i]['application_address'],
+                        amount: 100000,
+                        suggestedParams: {
+                            ...params,
+                            fee: 1000,
+                            flatFee: true
+                        }
+                    })
+                });
+            }
+
+            if (composer.count() > 0) {
                 await testnet.execute(composer);
-
-                primes[i]['application_version'] = version;;
 
                 config['gen2']['inputs']['primes'] = primes;
                 fs.writeFileSync('src/deploy/testnet/config.json', JSON.stringify(config, null, 4));
 
                 console.log('update main application ' + i);
+                console.log('fund main application ' + i);
+                console.log('initialize main application ' + i);
+                console.log('populate main application ' + i);
+                console.log('load main application ' + i);
             }
         }
     } catch (error) {
--- src/deploy/testnet/gen2/run.js
+++ src/deploy/testnet/gen2/run.js
@@ -10,12 +10,8 @@ exports.execute = async function () {
     console.log('gen2 prime setup contracts');
     await require('./prime/03_create_main_applications').execute();
     await require('./prime/04_update_main_applications').execute();
-    await require('./prime/05_fund_main_applications').execute();
-    await require('./prime/06_initialize_main_applications').execute();
-    await require('./prime/07_populate_main_applications').execute();
-    await require('./prime/08_load_main_applications').execute();
-    await require('./prime/09_lock_main_applications').execute();
-    await require('./prime/10_create_sub_applications').execute();
-    await require('./prime/11_update_sub_applications').execute();
+    await require('./prime/05_lock_main_applications').execute();
+    await require('./prime/06_create_sub_applications').execute();
+    await require('./prime/07_update_sub_applications').execute();
 
 }
