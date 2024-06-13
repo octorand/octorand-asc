c5e92df7dc9c11161a004785e03a364cf5c7ff6b
Declan Rocco <declanrocco@protonmail.com>
2024-06-13 13:57:12 +1000

    Update test commands

---
+++ src/deploy/devnet/gen2/prime/24_call_claim.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const devnet = require('./../../../../chain/devnet');
+const helpers = require('./../../../../chain/util/helpers');
+
+exports.execute = async function () {
+    try {
+        let connection = await devnet.get();
+        let params = await connection.algodClient.getTransactionParams().do();
+        let sender = connection.player.addr;
+        let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.player);
+
+        let config = JSON.parse(fs.readFileSync('src/deploy/devnet/config.json'));
+        let contract = new connection.baseClient.ABIContract(JSON.parse(fs.readFileSync('src/build/devnet/gen2/prime/claim/contract.json')));
+
+        let application = config['gen2']['contracts']['prime']['claim'];
+        let prime = config['gen2']['inputs']['prime'];
+
+        if (!application['claim']) {
+            let composer = new connection.baseClient.AtomicTransactionComposer();
+
+            composer.addMethodCall({
+                sender: sender,
+                signer: signer,
+                appID: application['application_id'],
+                method: helpers.method(contract, 'claim'),
+                methodArgs: [
+                    config['gen2']['contracts']['prime']['app']['application_id'],
+                ],
+                appForeignAssets: [
+                    prime['prime_asset_id'],
+                ],
+                suggestedParams: {
+                    ...params,
+                    fee: 2000,
+                    flatFee: true
+                }
+            });
+
+            await devnet.execute(composer);
+
+            application['claim'] = true;
+
+            config['gen2']['contracts']['prime']['claim'] = application;
+            fs.writeFileSync('src/deploy/devnet/config.json', JSON.stringify(config, null, 4));
+
+            console.log('called claim method');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/deploy/devnet/gen2/run.js
+++ src/deploy/devnet/gen2/run.js
@@ -19,17 +19,19 @@ exports.execute = async function () {
     await require('./prime/11_lock_prime_asset').execute();
 
     console.log('gen2 prime call functions');
-    // await require('./prime/12_call_upgrade').execute();
-    // await require('./prime/13_call_mint').execute();
-    // await require('./prime/14_call_withdraw').execute();
-    // await require('./prime/15_call_list').execute();
-    // await require('./prime/16_call_unlist').execute();
-    // await require('./prime/17_call_list').execute();
-    // await require('./prime/18_call_buy').execute();
-    // await require('./prime/19_call_rename').execute();
-    // await require('./prime/20_call_repaint').execute();
-    // await require('./prime/21_call_optin').execute();
-    // await require('./prime/22_call_optout').execute();
+    await require('./prime/12_update_main_application').execute();
+    await require('./prime/13_call_upgrade').execute();
+    await require('./prime/14_call_mint').execute();
+    await require('./prime/15_call_withdraw').execute();
+    await require('./prime/16_call_list').execute();
+    await require('./prime/17_call_unlist').execute();
+    await require('./prime/18_call_list').execute();
+    await require('./prime/19_call_buy').execute();
+    await require('./prime/20_call_rename').execute();
+    await require('./prime/21_call_repaint').execute();
+    await require('./prime/22_call_optin').execute();
+    await require('./prime/23_call_optout').execute();
+    await require('./prime/24_call_claim').execute();
 
     console.log('gen2 prime read states');
     // await require('./prime/23_read_logs').execute();
