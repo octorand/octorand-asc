ff16cb75ee888a02f7081d8dce6182fddb289979
Declan Rocco <declanrocco@protonmail.com>
2024-06-17 19:55:49 +1000

    Transfer asset

---
+++ src/utility/transfer_asset.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const algosdk = require("algosdk");
+
+(async () => {
+    try {
+        let server = process.env.TESTNET_ALGO_SERVER;
+        let mnemonic = process.env.TESTNET_LEGACY_MNEMONIC;
+
+        let client = new algosdk.Algodv2('', server, '');
+        let user = algosdk.mnemonicToSecretKey(mnemonic);
+        let sender = user.addr;
+        let signer = algosdk.makeBasicAccountTransactionSigner(user);
+        let params = await client.getTransactionParams().do();
+
+        let receiver = '';
+        let assetId = 0;
+        let amount = 1;
+
+        let composer = new algosdk.AtomicTransactionComposer();
+
+        composer.addTransaction({
+            signer: signer,
+            txn: algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+                from: sender,
+                to: receiver,
+                assetIndex: assetId,
+                amount: amount,
+                suggestedParams: {
+                    ...params,
+                    fee: 1000,
+                    flatFee: true
+                }
+            })
+        });
+
+        await composer.execute(client, 10);
+    } catch (error) {
+        console.log(error);
+    }
+})();
