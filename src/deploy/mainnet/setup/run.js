8eb1b971368b9afab0960c22d35476e0d214b782
Declan Rocco <declanrocco@protonmail.com>
2024-07-02 02:03:42 +1000

    Setup platform details

--- src/deploy/mainnet/setup/run.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-exports.execute = async function () {
-
-    console.log('setup asset requirements');
-    await require('./asset/00_create_platform_asset').execute();
-
-}
