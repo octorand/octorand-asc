bb208c6d519087f92834e29638500175505c24b7
Declan Rocco <declanrocco@protonmail.com>
2024-07-02 04:12:00 +1000

    Start minting primes

--- src/deploy/mainnet/gen2/prime/01_create_prime_assets.js
+++ src/deploy/mainnet/gen2/prime/01_create_prime_assets.js
@@ -7,15 +7,15 @@ exports.execute = async function () {
 
     let connection = await mainnet.get();
     let params = await connection.algodClient.getTransactionParams().do();
-    let sender = connection.gen1.addr;
-    let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.gen1);
+    let sender = connection.gen2.addr;
+    let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.gen2);
 
     let config = JSON.parse(fs.readFileSync('src/deploy/mainnet/config.json'));
 
-    let max = config['gen1']['inputs']['max'];
+    let max = config['gen2']['inputs']['max'];
 
     for (let i = 0; i < max; i++) {
-        let primes = config['gen1']['inputs']['primes'];
+        let primes = config['gen2']['inputs']['primes'];
 
         if (!primes[i]['prime_asset_id']) {
             let composer = new connection.baseClient.AtomicTransactionComposer();
@@ -28,9 +28,9 @@ exports.execute = async function () {
                     decimals: 0,
                     defaultFrozen: false,
                     manager: sender,
-                    reserve: sender,
-                    unitName: 'OG1-' + String(primes[i]['id']).padStart(3, '0'),
-                    assetName: 'Octorand Gen1 #' + String(primes[i]['id']).padStart(3, '0'),
+                    reserve: config['setup']['ipfs']['reserve'],
+                    unitName: 'OG2-' + String(primes[i]['id']).padStart(4, '0'),
+                    assetName: 'Octorand Gen2 #' + String(primes[i]['id']).padStart(4, '0'),
                     assetURL: 'template-ipfs://{ipfscid:0:dag-pb:reserve:sha2-256}',
                     suggestedParams: {
                         ...params,
@@ -45,7 +45,7 @@ exports.execute = async function () {
 
             primes[i]['prime_asset_id'] = asset_id;
 
-            config['gen1']['inputs']['primes'] = primes;
+            config['gen2']['inputs']['primes'] = primes;
             fs.writeFileSync('src/deploy/mainnet/config.json', JSON.stringify(config, null, 4));
 
             console.log('create prime asset ' + i);
