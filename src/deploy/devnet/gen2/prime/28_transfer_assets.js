60f7fd925486d00e6743eff4f4bf5ca5e487fd4e
Declan Rocco <declanrocco@protonmail.com>
2024-07-03 15:25:39 +1000

    Test moving assets

---
+++ src/deploy/devnet/gen2/prime/28_transfer_assets.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const devnet = require('./../../../../chain/devnet');
+
+exports.execute = async function () {
+    try {
+        let connection = await devnet.get();
+        let params = await connection.algodClient.getTransactionParams().do();
+        let sender = connection.player.addr;
+        let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.player);
+
+        let config = JSON.parse(fs.readFileSync('src/deploy/devnet/config.json'));
+
+        let application = config['gen2']['contracts']['prime']['app'];
+        let prime = config['gen2']['inputs']['prime'];
+
+        if (!application['retransferred']) {
+
+            let composer = new connection.baseClient.AtomicTransactionComposer();
+
+            composer.addTransaction({
+                sender: sender,
+                signer: signer,
+                txn: connection.baseClient.makeAssetTransferTxnWithSuggestedParamsFromObject({
+                    from: sender,
+                    to: connection.admin.addr,
+                    assetIndex: prime['prime_asset_id'],
+                    amount: 1,
+                    suggestedParams: {
+                        ...params,
+                        fee: 1000,
+                        flatFee: true
+                    }
+                })
+            });
+
+            await devnet.execute(composer);
+
+            application['retransferred'] = true;
+
+            config['gen2']['contracts']['prime']['app'] = application;
+            fs.writeFileSync('src/deploy/devnet/config.json', JSON.stringify(config, null, 4));
+
+            console.log('retransferred assets');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
