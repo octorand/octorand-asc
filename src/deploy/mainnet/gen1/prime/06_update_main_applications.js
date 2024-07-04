ce20a20f274520a3cbe45c55f0659f70676d15b2
Declan Rocco <declanrocco@protonmail.com>
2024-07-04 11:17:08 +1000

    Update applications

--- src/deploy/mainnet/gen1/prime/06_update_main_applications.js
+++ src/deploy/mainnet/gen1/prime/06_update_main_applications.js
@@ -20,109 +20,114 @@ exports.execute = async function () {
     for (let i = 0; i < max; i++) {
         let primes = config['gen1']['inputs']['primes'];
 
-        let params = await connection.algodClient.getTransactionParams().do();
-        let composer = new connection.baseClient.AtomicTransactionComposer();
+        if (!primes[i]['updated']) {
 
-        if (!primes[i]['funded']) {
-            primes[i]['funded'] = true;
+            let params = await connection.algodClient.getTransactionParams().do();
+            let composer = new connection.baseClient.AtomicTransactionComposer();
 
-            composer.addTransaction({
-                signer: signer,
-                txn: connection.baseClient.makePaymentTxnWithSuggestedParamsFromObject({
-                    from: sender,
-                    to: primes[i]['application_address'],
-                    amount: 400000,
+            if (!primes[i]['funded']) {
+                primes[i]['funded'] = true;
+
+                composer.addTransaction({
+                    signer: signer,
+                    txn: connection.baseClient.makePaymentTxnWithSuggestedParamsFromObject({
+                        from: sender,
+                        to: primes[i]['application_address'],
+                        amount: 400000,
+                        suggestedParams: {
+                            ...params,
+                            fee: 1000,
+                            flatFee: true
+                        }
+                    })
+                });
+            }
+
+            if (!primes[i]['initialized']) {
+                primes[i]['initialized'] = true;
+
+                composer.addMethodCall({
+                    sender: sender,
+                    signer: signer,
+                    appID: primes[i]['application_id'],
+                    method: helpers.method(contract, 'initialize'),
+                    methodArgs: [
+                        primes[i]['id'],
+                        config['setup']['platform']['asset_id'],
+                        primes[i]['prime_asset_id'],
+                        primes[i]['legacy_asset_id'],
+                    ],
+                    suggestedParams: {
+                        ...params,
+                        fee: 4000,
+                        flatFee: true
+                    }
+                });
+            }
+
+            if (!primes[i]['populated']) {
+                primes[i]['populated'] = true;
+
+                composer.addMethodCall({
+                    sender: sender,
+                    signer: signer,
+                    appID: primes[i]['application_id'],
+                    method: helpers.method(contract, 'populate'),
+                    methodArgs: [
+                        primes[i]['theme'],
+                        primes[i]['skin'],
+                        primes[i]['is_founder'],
+                        primes[i]['is_artifact'],
+                        primes[i]['is_pioneer'],
+                        primes[i]['is_explorer'],
+                        primes[i]['score'],
+                        primes[i]['sales'],
+                        primes[i]['drains'],
+                        primes[i]['transforms'],
+                        helpers.bytes(primes[i]['name'], 8),
+                        primes[i]['owner'],
+                    ],
                     suggestedParams: {
                         ...params,
                         fee: 1000,
                         flatFee: true
                     }
-                })
-            });
-        }
+                });
+            }
 
-        if (!primes[i]['initialized']) {
-            primes[i]['initialized'] = true;
+            if (primes[i]['application_version'] < version) {
+                primes[i]['application_version'] = version;
 
-            composer.addMethodCall({
-                sender: sender,
-                signer: signer,
-                appID: primes[i]['application_id'],
-                method: helpers.method(contract, 'initialize'),
-                methodArgs: [
-                    primes[i]['id'],
-                    config['setup']['platform']['asset_id'],
-                    primes[i]['prime_asset_id'],
-                    primes[i]['legacy_asset_id'],
-                ],
-                suggestedParams: {
-                    ...params,
-                    fee: 4000,
-                    flatFee: true
-                }
-            });
-        }
+                let approvalProgram = fs.readFileSync('src/build/mainnet/gen1/prime/app/approval.teal', 'utf8');
+                let clearProgram = fs.readFileSync('src/build/mainnet/gen1/prime/app/clear.teal', 'utf8');
 
-        if (!primes[i]['populated']) {
-            primes[i]['populated'] = true;
+                composer.addTransaction({
+                    signer: signer,
+                    txn: connection.baseClient.makeApplicationUpdateTxnFromObject({
+                        from: sender,
+                        appIndex: primes[i]['application_id'],
+                        onComplete: connection.baseClient.OnApplicationComplete.NoOpOC,
+                        approvalProgram: await mainnet.compile(approvalProgram),
+                        clearProgram: await mainnet.compile(clearProgram),
+                        suggestedParams: {
+                            ...params,
+                            fee: 1000,
+                            flatFee: true
+                        }
+                    })
+                });
+            }
 
-            composer.addMethodCall({
-                sender: sender,
-                signer: signer,
-                appID: primes[i]['application_id'],
-                method: helpers.method(contract, 'populate'),
-                methodArgs: [
-                    primes[i]['theme'],
-                    primes[i]['skin'],
-                    primes[i]['is_founder'],
-                    primes[i]['is_artifact'],
-                    primes[i]['is_pioneer'],
-                    primes[i]['is_explorer'],
-                    primes[i]['score'],
-                    primes[i]['sales'],
-                    primes[i]['drains'],
-                    primes[i]['transforms'],
-                    helpers.bytes(primes[i]['name'], 8),
-                    primes[i]['owner'],
-                ],
-                suggestedParams: {
-                    ...params,
-                    fee: 1000,
-                    flatFee: true
-                }
-            });
-        }
+            if (composer.count() > 0) {
+                await mainnet.execute(composer);
 
-        if (primes[i]['application_version'] < version) {
-            primes[i]['application_version'] = version;
+                primes[i]['updated'] = true;
 
-            let approvalProgram = fs.readFileSync('src/build/mainnet/gen1/prime/app/approval.teal', 'utf8');
-            let clearProgram = fs.readFileSync('src/build/mainnet/gen1/prime/app/clear.teal', 'utf8');
+                config['gen1']['inputs']['primes'] = primes;
+                fs.writeFileSync('src/deploy/mainnet/config.json', JSON.stringify(config, null, 4));
 
-            composer.addTransaction({
-                signer: signer,
-                txn: connection.baseClient.makeApplicationUpdateTxnFromObject({
-                    from: sender,
-                    appIndex: primes[i]['application_id'],
-                    onComplete: connection.baseClient.OnApplicationComplete.NoOpOC,
-                    approvalProgram: await mainnet.compile(approvalProgram),
-                    clearProgram: await mainnet.compile(clearProgram),
-                    suggestedParams: {
-                        ...params,
-                        fee: 1000,
-                        flatFee: true
-                    }
-                })
-            });
-        }
-
-        if (composer.count() > 0) {
-            await mainnet.execute(composer);
-
-            config['gen1']['inputs']['primes'] = primes;
-            fs.writeFileSync('src/deploy/mainnet/config.json', JSON.stringify(config, null, 4));
-
-            console.log('update main application ' + i);
+                console.log('update main application ' + i);
+            }
         }
     }
 }
