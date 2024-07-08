e58cf203bd7102bb45408a454358d3e07209c154
Declan Rocco <declanrocco@protonmail.com>
2024-07-09 01:10:43 +1000

    Save graphic files

--- src/deploy/mainnet/gen1/prime/15_update_graphic_svgs.js
+++ src/deploy/mainnet/gen1/prime/15_update_graphic_svgs.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const fs = require('fs');
+const builder = require('svg-builder');
 const colors = require('./../../../../chain/util/colors');
 
 exports.execute = async function () {
@@ -9,7 +10,17 @@ exports.execute = async function () {
     let primes = graphics['primes'];
 
     for (let i = 0; i < primes.length; i++) {
+        let prime = primes[i];
+
+        let svg = builder;
+        svg = svg.height(200);
+        svg = svg.width(200);
+
+        fs.writeFileSync('src/deploy/mainnet/gen1/prime/graphics/svg/' + prime.id + '.svg', svg.render());
+
         console.log('updated graphic svg ' + i);
+
+        break;
     }
 
     console.log('updated graphic svgs');
