ae6d38ebc3a68958a9fc94fff85c88c526017288
Declan Rocco <declanrocco@protonmail.com>
2024-07-09 13:57:48 +1000

    Upload prime pngs

---
+++ src/deploy/mainnet/gen2/prime/17_upload_graphic_pngs.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const cids = require('cids')
+const forms = require('form-data');
+const fs = require('fs');
+const mainnet = require('./../../../../chain/mainnet');
+
+exports.execute = async function () {
+
+    let connection = await mainnet.get();
+
+    let config = JSON.parse(fs.readFileSync('src/deploy/mainnet/config.json'));
+
+    let max = config['gen2']['inputs']['max'];
+
+    for (let i = 0; i < max; i++) {
+        let primes = config['gen2']['inputs']['primes'];
+
+        let source = 'src/deploy/mainnet/gen2/prime/graphics/png/' + primes[i].id + '.png';
+        let token = Buffer.from(process.env.INFURA_PROJECT_ID + ':' + process.env.INFURA_PROJECT_SECRET).toString('base64');
+
+        var data = new forms();
+        data.append('file', fs.createReadStream(source));
+
+        var request = {
+            method: 'post',
+            url: 'https://ipfs.infura.io:5001/api/v0/add',
+            headers: {
+                'Authorization': 'Basic ' + token,
+                ...data.getHeaders()
+            },
+            data: data
+        };
+
+        const res = await axios(request);
+
+        let cid = new cids(res.data.Hash);
+        let hash = Buffer.from(cid.multihash.slice(-32));
+        let reserve = connection.baseClient.encodeAddress(Uint8Array.from(Buffer.from(hash)));
+
+        if (primes[i]['asset_reserve'] != reserve) {
+            primes[i]['asset_cid'] = res.data.Hash;
+            primes[i]['asset_reserve'] = reserve;
+            primes[i]['asset_version'] = 0;
+
+            config['gen2']['inputs']['primes'] = primes;
+            fs.writeFileSync('src/deploy/mainnet/config.json', JSON.stringify(config, null, 4));
+
+            console.log('uploaded graphic png ' + i);
+        }
+    }
+
+    console.log('uploaded graphic pngs');
+}
+
