ebc1cc5cf7de04f559f07d6bd191d7ca0ba48729
Declan Rocco <declanrocco@protonmail.com>
2024-08-07 04:02:32 +1000

    Add release scripts

---
+++ src/deploy/mainnet/launchpad/guardians/run.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+exports.execute = async function () {
+
+    console.log('launchpad guardians item setup contracts');
+    // await require('./item/00_create_item_inputs').execute();
+    // await require('./item/01_create_item_assets').execute();
+
+    console.log('launchpad guardians item setup resources');
+    // await require('./item/02_create_main_applications').execute();
+    // await require('./item/03_update_main_applications').execute();
+    // await require('./item/04_create_sub_applications').execute();
+    // await require('./item/05_update_sub_applications').execute();
+    // await require('./item/06_refresh_main_applications').execute();
+}
