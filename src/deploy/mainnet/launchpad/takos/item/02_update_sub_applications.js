60268117659b0e35bfaa6b3746f84ca39ae26479
Declan Rocco <declanrocco@protonmail.com>
2024-08-07 04:41:23 +1000

    Create sub applications

---
+++ src/deploy/mainnet/launchpad/takos/item/02_update_sub_applications.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const mainnet = require('./../../../../../chain/mainnet');
+
+exports.execute = async function () {
+
+    let connection = await mainnet.get();
+    let params = await connection.algodClient.getTransactionParams().do();
+    let sender = connection.admin.addr;
+    let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.admin);
+
+    let config = JSON.parse(fs.readFileSync('src/deploy/mainnet/config.json'));
+
+    let version = 1;
+
+    let contracts = ['buy', 'claim', 'list', 'mint', 'rename', 'unlist'];
+
+    for (let i = 0; i < contracts.length; i++) {
+        let contract = contracts[i];
+
+        let application = config['launchpad']['takos']['contracts']['item'][contract];
+
+        if (application['application_version'] < version) {
+            let approvalProgram = fs.readFileSync('src/build/mainnet/launchpad/takos/item/' + contract + '/approval.teal', 'utf8');
+            let clearProgram = fs.readFileSync('src/build/mainnet/launchpad/takos/item/' + contract + '/clear.teal', 'utf8');
+
+            let composer = new connection.baseClient.AtomicTransactionComposer();
+
+            composer.addTransaction({
+                signer: signer,
+                txn: connection.baseClient.makeApplicationUpdateTxnFromObject({
+                    from: sender,
+                    appIndex: application['application_id'],
+                    onComplete: connection.baseClient.OnApplicationComplete.NoOpOC,
+                    approvalProgram: await mainnet.compile(approvalProgram),
+                    clearProgram: await mainnet.compile(clearProgram),
+                    suggestedParams: {
+                        ...params,
+                        fee: 1000,
+                        flatFee: true
+                    }
+                })
+            });
+
+            await mainnet.execute(composer);
+
+            application['application_version'] = version;
+
+            config['launchpad']['takos']['contracts']['item'][contract] = application;
+            fs.writeFileSync('src/deploy/mainnet/config.json', JSON.stringify(config, null, 4));
+
+            console.log('updated item ' + contract);
+        }
+    }
+
+}
--- src/deploy/mainnet/launchpad/takos/run.js
+++ src/deploy/mainnet/launchpad/takos/run.js
@@ -6,5 +6,6 @@ exports.execute = async function () {
     await require('./item/00_create_item_inputs').execute();
 
     console.log('launchpad takos item setup resources');
-
+    await require('./item/01_create_sub_applications').execute();
+    await require('./item/02_update_sub_applications').execute();
 }
