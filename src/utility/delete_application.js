8a6789dbe80f1615908b82e12d09ba50ea8476fd
Declan Rocco <declanrocco@protonmail.com>
2024-08-07 18:20:19 +1000

    Delete application script

---
+++ src/utility/delete_application.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const algosdk = require("algosdk");
+
+(async () => {
+    try {
+        let server = process.env.TESTNET_ALGO_SERVER;
+        let mnemonic = process.env.TESTNET_GEN1_MANAGER_MNEMONIC;
+        let application = 0;
+
+        let client = new algosdk.Algodv2('', server, '');
+        let user = algosdk.mnemonicToSecretKey(mnemonic);
+        let sender = user.addr;
+        let signer = algosdk.makeBasicAccountTransactionSigner(user);
+        let params = await client.getTransactionParams().do();
+
+        let composer = new algosdk.AtomicTransactionComposer();
+
+        composer.addTransaction({
+            signer: signer,
+            txn: algosdk.makeApplicationDeleteTxnFromObject({
+                from: sender,
+                appIndex: application,
+                suggestedParams: {
+                    ...params,
+                    fee: 1000,
+                    flatFee: true
+                }
+            })
+        });
+
+        await composer.execute(client, 10);
+    } catch (error) {
+        console.log(error);
+    }
+})();
