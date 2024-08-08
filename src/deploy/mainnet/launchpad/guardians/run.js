5c8111909f9c8dfca2aa6d79a5f6d462c8aa6d33
Declan Rocco <declanrocco@protonmail.com>
2024-08-09 06:04:51 +1000

    Update legacy apps

--- src/deploy/mainnet/launchpad/guardians/run.js
+++ src/deploy/mainnet/launchpad/guardians/run.js
@@ -10,4 +10,5 @@ exports.execute = async function () {
     await require('./item/02_update_sub_applications').execute();
     await require('./item/03_create_main_applications').execute();
     await require('./item/04_update_main_applications').execute();
+    await require('./item/05_update_legacy_applications').execute();
 }
