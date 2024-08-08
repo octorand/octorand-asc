97ce2bd4ee2093691c9866d1d70f10d37ff1dfa3
Declan Rocco <declanrocco@protonmail.com>
2024-08-08 10:03:50 +1000

    Update or delete application

--- src/utility/update_application.js
+++ src/utility/update_application.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 
 const algosdk = require("algosdk");
+const fs = require('fs');
+const mainnet = require('./../chain/mainnet');
 
 (async () => {
     try {
         let server = process.env.TESTNET_ALGO_SERVER;
         let mnemonic = process.env.TESTNET_ADMIN_MNEMONIC;
         let application = 0;
+        let approval = 'src/build/xxx/approval.teal';
+        let clear = 'src/build/xxx/clear.teal';
 
         let client = new algosdk.Algodv2('', server, '');
         let user = algosdk.mnemonicToSecretKey(mnemonic);
@@ -16,11 +20,17 @@ const algosdk = require("algosdk");
 
         let composer = new algosdk.AtomicTransactionComposer();
 
+        let approvalProgram = fs.readFileSync(approval, 'utf8');
+        let clearProgram = fs.readFileSync(clear, 'utf8');
+
         composer.addTransaction({
             signer: signer,
-            txn: algosdk.makeApplicationDeleteTxnFromObject({
+            txn: algosdk.makeApplicationUpdateTxnFromObject({
                 from: sender,
                 appIndex: application,
+                onComplete: algosdk.OnApplicationComplete.NoOpOC,
+                approvalProgram: await mainnet.compile(approvalProgram),
+                clearProgram: await mainnet.compile(clearProgram),
                 suggestedParams: {
                     ...params,
                     fee: 1000,
