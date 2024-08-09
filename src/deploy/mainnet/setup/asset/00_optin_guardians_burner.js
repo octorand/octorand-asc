ac38903bea9a9f499dd098592692a2de69323908
Declan Rocco <declanrocco@protonmail.com>
2024-08-09 17:15:12 +1000

    Setup asset burners

---
+++ src/deploy/mainnet/setup/asset/00_optin_guardians_burner.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const mainnet = require('./../../../../chain/mainnet');
+const helpers = require('./../../../../chain/util/helpers');
+
+exports.execute = async function () {
+    try {
+        let connection = await mainnet.get();
+        let params = await connection.algodClient.getTransactionParams().do();
+        let sender = connection.admin.addr;
+        let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.admin);
+
+        let contract = new connection.baseClient.ABIContract({
+            "name": "ARC54",
+            "desc": "Standardized application for burning ASAs",
+            "methods": [
+                {
+                    "name": "arc54_optIntoASA",
+                    "args": [
+                        {
+                            "name": "asa",
+                            "type": "asset",
+                            "desc": "The asset to which the contract will opt in"
+                        }
+                    ],
+                    "desc": "A method to opt the contract into an ASA",
+                    "returns": {
+                        "type": "void",
+                        "desc": ""
+                    }
+                },
+                {
+                    "name": "createApplication",
+                    "desc": "",
+                    "returns": {
+                        "type": "void",
+                        "desc": ""
+                    },
+                    "args": []
+                }
+            ]
+        });
+
+        let config = JSON.parse(fs.readFileSync('src/deploy/mainnet/config.json'));
+
+        let opted = config['setup']['guardians']['optin_burner'];
+
+        if (!opted) {
+
+            let composer = new connection.baseClient.AtomicTransactionComposer();
+
+            composer.addTransaction({
+                signer: signer,
+                txn: connection.baseClient.makePaymentTxnWithSuggestedParamsFromObject({
+                    from: sender,
+                    to: process.env.MAINNET_BURNER_APPLICATION_ADDRESS,
+                    amount: 200000,
+                    suggestedParams: {
+                        ...params,
+                        fee: 1000,
+                        flatFee: true
+                    }
+                })
+            });
+
+            composer.addMethodCall({
+                sender: sender,
+                signer: signer,
+                appID: Number(process.env.MAINNET_BURNER_APPLICATION_ID),
+                method: helpers.method(contract, 'arc54_optIntoASA'),
+                methodArgs: [
+                    Number(config['setup']['guardians']['asset_id']),
+                ],
+                suggestedParams: {
+                    ...params,
+                    fee: 2000,
+                    flatFee: true
+                }
+            });
+
+            await mainnet.execute(composer);
+
+            config['setup']['guardians']['optin_burner'] = true;
+            fs.writeFileSync('src/deploy/mainnet/config.json', JSON.stringify(config, null, 4));
+
+            console.log('opted in guardians burner');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/deploy/mainnet/setup/run.js
+++ src/deploy/mainnet/setup/run.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 exports.execute = async function () {
 
+    console.log('setup asset requirements');
+    await require('./asset/00_optin_guardians_burner').execute();
+    await require('./asset/01_optin_takos_burner').execute();
+
     console.log('setup ipfs requirements');
     await require('./ipfs/00_generate_reserve_address').execute();
 
