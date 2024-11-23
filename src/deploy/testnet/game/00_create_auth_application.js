9b0b6b9d59dd00e68541161c124917d4ff5148b2
Declan Rocco <declanrocco@protonmail.com>
2024-11-24 06:15:45 +1000

    Generate testnet game contracts

---
+++ src/deploy/testnet/game/00_create_auth_application.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const testnet = require('./../../../chain/testnet');
+
+exports.execute = async function () {
+    try {
+        let connection = await testnet.get();
+        let params = await connection.algodClient.getTransactionParams().do();
+        let sender = connection.admin.addr;
+        let signer = connection.baseClient.makeBasicAccountTransactionSigner(connection.admin);
+
+        let config = JSON.parse(fs.readFileSync('src/deploy/testnet/config.json'));
+
+        let application = config['game']['contracts']['auth'];
+
+        if (!application['application_id']) {
+            let approvalProgram = fs.readFileSync('src/build/testnet/game/auth/approval.teal', 'utf8');
+            let clearProgram = fs.readFileSync('src/build/testnet/game/auth/clear.teal', 'utf8');
+
+            let composer = new connection.baseClient.AtomicTransactionComposer();
+
+            composer.addTransaction({
+                signer: signer,
+                txn: connection.baseClient.makeApplicationCreateTxnFromObject({
+                    from: sender,
+                    onComplete: connection.baseClient.OnApplicationComplete.NoOpOC,
+                    approvalProgram: await testnet.compile(approvalProgram),
+                    clearProgram: await testnet.compile(clearProgram),
+                    numLocalInts: 0,
+                    numLocalByteSlices: 0,
+                    numGlobalInts: 0,
+                    numGlobalByteSlices: 1,
+                    extraPages: 0,
+                    suggestedParams: {
+                        ...params,
+                        fee: 1000,
+                        flatFee: true
+                    }
+                })
+            });
+
+            let response = await testnet.execute(composer);
+            let application_id = response.information['application-index'];
+
+            application['application_id'] = application_id;
+            application['application_address'] = connection.baseClient.getApplicationAddress(application_id);
+            application['application_version'] = 0;
+
+            config['game']['contracts']['auth'] = application;
+            fs.writeFileSync('src/deploy/testnet/config.json', JSON.stringify(config, null, 4));
+
+            console.log('created game auth');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
